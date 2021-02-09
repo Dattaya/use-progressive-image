38d3ee530e2c670f258717971c751f4d74636c24
Yaroslav Kiliba <om.dattaya@gmail.com>
2021-02-09 22:18:42 +0200

    chore: increase intercept delay in a test

--- cypress/integration/img.spec.js
+++ cypress/integration/img.spec.js
@@ -9,7 +9,7 @@ context('Test SSR', () => {
 
   it('No mismatch between the server and client', () => {
     cy.intercept('GET', 'http://localhost:3000/img.png', {
-      delayMs: 1,
+      delayMs: 10,
     })
     cy.visit(`/img`)
     cy.get('#img').should('have.attr', 'src', `/img.png`)
