83eed46c455abecbeeb83a2b1b39341a698c2366
Yaroslav Kiliba <om.dattaya@gmail.com>
2021-02-14 22:13:13 +0200

    chore: test in production

--- cypress/integration/img.spec.js
+++ cypress/integration/img.spec.js
@@ -1,21 +1,12 @@
 /// <reference types="cypress" />
 
 context('Test SSR', () => {
-  beforeEach(() => {
+  it('No mismatch between the server and client', () => {
+    cy.intercept('GET', 'http://localhost:3000/img.png', {
+      delayMs: 10,
+    })
     cy.visit(`/img`)
-  })
-
-  it('Does not throw error in SSR', () => {
-    // cy.visit(`/img`, { qs: { img: imgParam, ssr: 1 } })
     cy.get('#img').should('have.attr', 'src', `/img.png`)
+    cy.get('#messages').should('have.text', 'not loading')
   })
-
-  // it('No mismatch between the server and client', () => {
-  //   cy.intercept('GET', 'http://localhost:3000/img.png', {
-  //     delayMs: 10,
-  //   })
-  //   cy.visit(`/img`)
-  //   cy.get('#img').should('have.attr', 'src', `/img.png`)
-  //   cy.get('#messages').should('have.text', 'not loading')
-  // })
 })
